c9c75965af035b16c7662dbcd87277c90d72881b
Philippe Laurens <philippe.laurens64@sfr.fr>
2024-10-25 13:11:23 +0200

    update

--- 1-cluster/terraform.tfvars
+++ 1-cluster/terraform.tfvars
@@ -1,4 +1,3 @@
 aws_profile             = "WorkloadProd"
-aws_region              = "us-east-1"
 environment             = "stag"
 business_divsion        = "hr"
--- 1-cluster/variables.tf
+++ 1-cluster/variables.tf
@@ -2,7 +2,6 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "us-west-2"  
 }
  
 # Terraform Profile
